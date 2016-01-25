6254158af719170dcb835b1a276fd49d017b943a
LehmanHe <935524068@qq.com>
2016-01-25 13:27:54 +0800

    Update DialogMenuItem.java

--- FlycoDialog_Lib/src/main/java/com/flyco/dialog/entity/DialogMenuItem.java
+++ FlycoDialog_Lib/src/main/java/com/flyco/dialog/entity/DialogMenuItem.java
@@ -3,9 +3,11 @@ package com.flyco.dialog.entity;
 public class DialogMenuItem {
     public String mOperName;
     public int mResId;
+    public int addressId;
 
-    public DialogMenuItem(String operName, int resId) {
+    public DialogMenuItem(String operName, int resId,int addressId) {
         mOperName = operName;
         mResId = resId;
+        this.addressId=addressId;
     }
 }
